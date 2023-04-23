d5680d88dba9d92d1f6fcf5e14e2bb953a08e35d
AshQuimby <ian.aaron.mcallister@gmail.com>
2023-04-23 00:25:36 -0700

    Bowl Boy gun hand works, time to add bullets.

--- src/sab/game/fighter/BowlBoy.java
+++ src/sab/game/fighter/BowlBoy.java
@@ -4,22 +4,28 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.math.Vector2;
 import com.seagull_engine.Seagraphics;
 import sab.game.Player;
+import sab.game.SABSounds;
 import sab.game.animation.Animation;
+import sab.net.Keys;
+
+import java.security.Key;
 
 public class BowlBoy extends FighterType {
 
-    Vector2 gunHandPosition;
+    private Vector2 gunHandPosition;
+    private boolean gunMode;
+    private int shootRecoil;
 
     @Override
     public void setDefaults(sab.game.fighter.Fighter fighter) {
         fighter.id = "bowl_boy";
         fighter.name = "Bowl Boy";
-        fighter.hitboxWidth = 40;
-        fighter.hitboxHeight = 60;
+        fighter.hitboxWidth = 36;
+        fighter.hitboxHeight = 46;
         fighter.renderWidth = 64;
         fighter.renderHeight = 64;
-        fighter.imageOffsetX = 0;
-        fighter.imageOffsetY = 2;
+        fighter.imageOffsetX = 2;
+        fighter.imageOffsetY = 9;
         fighter.frames = 20;
         fighter.jumpHeight = 160;
         fighter.friction = .15f;
@@ -29,6 +35,8 @@ public class BowlBoy extends FighterType {
         fighter.description = "Bowl Boy and his sister, Pot Head, are useless at just about everything. They entered a pact with a demon that gave them gun hands, but didn't solve their root problem of having strength, intelligence, and constitution not much better than their porcelain counterparts. As it is, they suck and nobody likes them.";
         fighter.debut = "Bowl Boy & Pot Head";
         gunHandPosition = new Vector2();
+
+        gunMode = false;
 
         fighter.walkAnimation = new Animation(4, 7, 4, true);
         fighter.idleAnimation = new Animation(0, 1, 16, true);
@@ -48,17 +56,49 @@ public class BowlBoy extends FighterType {
         } else {
 
         }
+
+        if (gunMode && player.keys.isPressed(Keys.ATTACK)) {
+            gunMode = true;
+        } else {
+            gunMode = false;
+        }
+        if (shootRecoil > 0) {
+            shootRecoil--;
+        }
+        if (gunMode) {
+            if (shootRecoil <= 0) {
+                shootRecoil = 10;
+                SABSounds.playSound("crunch.mp3");
+                player.occupy(10);
+            }
+        }
+
+        if (gunMode && player.frame < 2) {
+            player.frame = 2;
+        }
+    }
+
+    public void shootBullet(Fighter fighter, Player player) {
+
+    }
+
+    // Probably the simplest neutral attack method in the game but also the most complex neutral attack
+    @Override
+    public void neutralAttack(Fighter fighter, Player player) {
+        if (!player.usedRecovery) {
+            gunMode = true;
+        }
     }
 
     @Override
     public void render(Fighter fighter, Player player, Seagraphics g) {
-        super.render(fighter, player, g);
-
-        // Happens in the character select screen
-        if (player.battle == null) {
-            g.usefulTintDraw(g.imageProvider.getImage("bowl_boy_gun_hands.png"), player.drawRect.x, player.drawRect.y, (int) player.drawRect.width, (int) player.drawRect.height, player.frame, fighter.frames, 0, player.direction == 1, false, player.getIFrames() / 10 % 2 == 0 ? Color.WHITE : new Color(1, 1, 1, 0.5f));
+        // player.battle == null so that it happens in the character select screen
+        if (gunMode || player.battle == null) {
+            g.usefulTintDraw(g.imageProvider.getImage("bowl_boy_gun_hands.png"), player.drawRect.x, player.drawRect.y, (int) player.drawRect.width, (int) player.drawRect.height, player.frame, fighter.frames, (int) (shootRecoil * player.direction * 1.5f), player.direction == 1, false, player.getIFrames() / 10 % 2 == 0 ? Color.WHITE : new Color(1, 1, 1, 0.5f));
         } else {
             g.usefulTintDraw(g.imageProvider.getImage("bowl_boy_hands.png"), player.drawRect.x, player.drawRect.y, (int) player.drawRect.width, (int) player.drawRect.height, player.frame, fighter.frames, 0, player.direction == 1, false, player.getIFrames() / 10 % 2 == 0 ? Color.WHITE : new Color(1, 1, 1, 0.5f));
         }
+
+        super.render(fighter, player, g);
     }
 }
